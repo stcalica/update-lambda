5aff26aa0a6d2138ea6ae5961c68f39f8afeac7c
Kyle Calica <kyle@getpostman.com>
2019-10-17 17:28:38 -0700

    added functions

---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+try {
+  const functionName = core.getInput('function-name');
+  const package = core.getInput('package');
+  const AWS_SECRET_KEY = core.setSecret(core.getInput('AWS_SECRET_KEY'));
+  const AWS_SECRET_ID = core.setSecret(core.getInput('AWS_SECRET_ID'));
+  const AWS_REGION = core.setSecret(core.getInput('AWS_REGION'));
+
+  console.log(`Updating Function Name ${functionName} with ${package}!`);
+  var zipBuffer = fs.readFileSync(`./${package}`);
+  core.debug('Package put into memory buffer');
+
+  const lambda = new AWS.Lambda({
+      apiVersion: '2015-03-31',
+      region: AWS_REGION, // Or whichever region you use
+      maxRetries: 3,
+      sslEnabled: true,
+      logger: console,
+  });
+
+  const params = {
+    FunctionName: functionName,
+    Publish: false,
+    ZipFile: zipBuffer,
+  };
+
+  lambda.updateFunctionCode(params, err => {
+      if (err) {
+          console.error(err);
+          process.exit(3);
+      }
+  });
+
+} catch (error) {
+  core.setFailed(error.message);
+}
